93392e6cb8c1c7b3e29087a373e6ac1c01511bf3
RajaJulie <julie.ratnan@gmail.com>
2026-05-18 16:16:08 +0200

    page login

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { LoginForm } from "@/components/login-form"
+import { GalleryVerticalEndIcon } from "lucide-react"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="relative hidden bg-muted lg:block">
+        
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            
+            
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
